e4bfba968026727d3c6df162d5d943fb8b68da86
aaratinerlekar <aaratinerlekar@gmail.com>
2022-06-10 13:15:41 +0530

    Added queries
     for filter student by course and filter not assigned course

--- src/main/java/com/school/schoolMgmt/repository/CourseRepository.java
+++ src/main/java/com/school/schoolMgmt/repository/CourseRepository.java
@@ -1,5 +1,7 @@
 package com.school.schoolMgmt.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,11 @@ public interface CourseRepository extends CrudRepository<Course, Long> {
 	
 	@Query("SELECT count(c) FROM Course c WHERE c.courseName = ?1")
 	int findByCourseName(String courseName);
+	
+	
+	@Query("SELECT c FROM Course c join CourseAssignment ca WHERE ca.student.sid = ?1")
+	Set<Course>findByStudentId(long studentId);
+	
+	@Query("SELECT c FROM Course c WHERE c.cid not in(select distinct course.cid from CourseAssignment)")
+	Set<Course>findNotAssignedCourses();
 }
