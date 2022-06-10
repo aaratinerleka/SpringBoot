f7b91c66e3cd6efb7178e426224c772425994b58
aaratinerlekar <aaratinerlekar@gmail.com>
2022-06-10 13:16:18 +0530

    added queries

--- src/main/java/com/school/schoolMgmt/repository/StudentRepository.java
+++ src/main/java/com/school/schoolMgmt/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.school.schoolMgmt.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -12,4 +14,9 @@ public interface StudentRepository extends CrudRepository<Student, Long> {
 	@Query("SELECT count(s) FROM Student s WHERE s.sname = ?1 and s.address = ?2")
 	int findByNameAndAddress(String sname,String address);
 
+	@Query("SELECT s FROM Student s join CourseAssignment ca WHERE ca.course.cid = ?1")
+	Set<Student>findByStudentId(long courseId);
+	
+	@Query("SELECT s FROM Student s WHERE s.sid not in(select distinct student.sid from CourseAssignment)")
+	Set<Student>findNotAssignedStudents();
 }
